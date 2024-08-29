ef123d947fc50e65cbb9414f89d876e740b6f8b6
tricksterd <kozavkolena@gmail.com>
2024-08-29 16:39:57 +0300

    added validation to form

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -11,6 +11,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +24,20 @@ class _NewItemState extends State<NewItem> {
         body: Padding(
           padding: const EdgeInsets.all(12),
           child: Form(
+            key: _formKey,
             child: Column(
               children: [
                 TextFormField(
                   maxLength: 50,
                   decoration: const InputDecoration(label: Text('Name')),
                   validator: (value) {
-                    return 'Demo...';
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return 'Must be between 1 and 50 charachers.';
+                    }
+                    return null;
                   },
                 ),
                 Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
@@ -33,6 +46,16 @@ class _NewItemState extends State<NewItem> {
                       decoration:
                           const InputDecoration(label: Text('Quantity')),
                       initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positive number.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(
@@ -58,6 +81,16 @@ class _NewItemState extends State<NewItem> {
                             ))
                     ], onChanged: (value) {}),
                   )
+                ]),
+                const SizedBox(height: 12),
+                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: const Text('Reset')),
+                  ElevatedButton(
+                      onPressed: _saveItem, child: const Text('Add Item'))
                 ])
               ],
             ),
